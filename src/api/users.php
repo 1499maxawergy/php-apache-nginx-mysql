0d6bb19c374f3a3a1f64f29b654f6c2276a648b3
Maxawergy <maxawergy@yandex.ru>
2022-10-12 00:22:56 +0300

    CODES ADDED (4 PRACT COMPLETE)

--- src/api/users.php
+++ src/api/users.php
@@ -20,8 +20,10 @@ switch($requestMethod){
         }
         if (count($answer) == 1){
             $answer["status"] = "Error. User(-s) not found.";
+            http_response_code(404);
         } else {
             $answer["status"] = "Success. User(-s) found";
+            http_response_code(200);
         }
         echo json_encode($answer);
         break;
@@ -36,15 +38,18 @@ switch($requestMethod){
             $result = $query_result->fetch_row();
             if (!empty($result)){
                 $answer["status"] = "Error. User with this username already exists.";
+                http_response_code(409);
             } else {
                 $password = crypt($password);
                 $stmt = $con->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
                 $stmt->bind_param('sss', $username, $password, $email);
                 $stmt->execute();
                 $answer["status"] = "Success. User created.";
+                http_response_code(200);
             }
         } else {
             $answer["status"] = "Error. Need username, password and email in JSON BODY.";
+            http_response_code(422);
         }
         echo json_encode($answer);
         break;
@@ -54,6 +59,7 @@ switch($requestMethod){
         if (!empty($obj->{'username'}) && !empty($obj->{'email'})){
             if (empty(isset($_GET['id']))){
                 $answer["status"] = "Error. Need ID Param";
+                http_response_code(422);
             } else {
                 $query_result = $con->query("SELECT * FROM users WHERE ID='".$_GET['id']."'");
                 $result = $query_result->fetch_row();
@@ -62,36 +68,44 @@ switch($requestMethod){
                     $result = $query_result->fetch_row();
                     if (!empty($result)){
                     $answer["status"] = "Error. User with this username already exists.";
+                    http_response_code(409);
                     } else {
                     $con->query("UPDATE users SET username='".$obj->{'username'}."', email='".$obj->{'email'}."' WHERE ID='".$_GET['id']."'");
                     $answer["status"] = "Success. User updated.";
+                    http_response_code(200);
                     }
                 } else {
                     $answer["status"] = "Error. User not found.";
+                    http_response_code(404);
                 }
             }
         } else {
             $answer["status"] = "Error. Need username and email in JSON BODY.";
+            http_response_code(422);
         }
         echo json_encode($answer);
         break;
     case 'DELETE':
         if (empty(isset($_GET['id']))){
             $answer["status"] = "Error. Need ID Param";
+            http_response_code(422);
         } else {
             $query_result = $con->query("SELECT * FROM users WHERE ID='".$_GET['id']."'");
             $result = $query_result->fetch_row();
             if (!empty($result)){
                 $query_result = $con->query("DELETE FROM users WHERE ID='".$_GET['id']."'");
                 $answer["status"] = "Success. User Deleted.";
+                http_response_code(200);
             } else {
                 $answer["status"] = "Error. User not found.";
+                http_response_code(404);
             }
         }
         echo json_encode($answer);
         break;
     default:
         $answer["status"] = "This REST Method not allowed.";
+        http_response_code(500);
         echo json_encode($answer);
         break;
 }
